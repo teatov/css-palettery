201e3d59aaa7b7d39713cf12bd9738469b5b2558
teatov <hlebpoimenigleb@gmail.com>
2024-12-30 09:01:39 +1000

    implement group color adjustments

--- src/App.svelte
+++ src/App.svelte
@@ -17,7 +17,16 @@
 
   type ColorRule = { name: string; sections: ColorSection[] };
 
-  type ModificationGroup = { name: string; colorItems: ColorItem[] };
+  type ModificationGroup = {
+    name: string;
+    colorItems: ColorItem[];
+    adjustHue: boolean;
+    hueAdjustment: number;
+    adjustSaturation: boolean;
+    saturationAdjustment: number;
+    adjustLightness: boolean;
+    lightnessAdjustment: number;
+  };
 
   let source = $state(exampleCss);
   let ast: css.Stylesheet | undefined = $state();
@@ -91,7 +100,16 @@
   }
 
   function newGroup() {
-    selectedGroup = { name: 'New group', colorItems: [] };
+    selectedGroup = {
+      name: 'New group',
+      colorItems: [],
+      adjustHue: false,
+      hueAdjustment: 0,
+      adjustSaturation: false,
+      saturationAdjustment: 0,
+      adjustLightness: false,
+      lightnessAdjustment: 0,
+    };
     groups.push(selectedGroup);
   }
 
@@ -113,24 +131,101 @@
       (item) => item !== colorItem
     );
     colorItem.group = null;
+    setColorItemColor(colorItem, colorItem.initialColor);
   }
+
+  function setColorItemColor(colorItem: ColorItem, color: chroma.Color) {
+    colorItem.color = color;
+    colorItem.declaration.value = color.toString();
+  }
+
+  function relativeValueString(value: number): string {
+    return (value >= 0 ? '+' : '') + value.toString();
+  }
+
+  function remap(
+    weight: number,
+    a1: number,
+    b1: number,
+    a2: number,
+    b2: number
+  ) {
+    return a2 + ((b2 - a2) * (weight - a1)) / (b1 - a1);
+  }
+
+  function adjustValue(
+    initialValue: number,
+    value: number,
+    max: number = 1,
+    min: number = 0
+  ): number {
+    if (value > 0) {
+      return remap(value, 0, 1, initialValue, max);
+    }
+    if (value < 0) {
+      return remap(value, -1, 0, min, initialValue);
+    }
+
+    return initialValue;
+  }
+
+  function updateGroupColors(group: ModificationGroup) {
+    group.colorItems.forEach((colorItem) => {
+      let color = colorItem.initialColor;
+
+      if (group.adjustHue) {
+        color = color.set('hsl.h', relativeValueString(group.hueAdjustment));
+      }
+      if (group.adjustSaturation) {
+        color = color.set(
+          'hsl.s',
+          adjustValue(color.hsl()[1], group.saturationAdjustment)
+        );
+      }
+      if (group.adjustLightness) {
+        color = color.set(
+          'hsl.l',
+          adjustValue(color.hsl()[2], group.lightnessAdjustment)
+        );
+      }
+
+      setColorItemColor(colorItem, color);
+    });
+  }
+
+  $effect(() => {
+    if (selectedGroup) {
+      selectedGroup.adjustHue;
+      selectedGroup.hueAdjustment;
+      selectedGroup.adjustSaturation;
+      selectedGroup.saturationAdjustment;
+      selectedGroup.adjustLightness;
+      selectedGroup.lightnessAdjustment;
+      updateGroupColors(selectedGroup);
+    }
+  });
 </script>
 
 {#snippet colorList(
   colorItems: ColorItem[],
-  functionOnClick: (colorItem: ColorItem) => void
+  functionOnClick: (colorItem: ColorItem) => void,
+  showGrouping: boolean = false
 )}
-  <ul class="grid grid-cols-8 gap-1 text-xs">
+  <ul class="grid grid-cols-8 text-xs font-mono break-words">
     {#each colorItems as colorItem}
       <li
         class="min-h-10 h-full {colorItem.color.hsl()[2] > 0.5 &&
         colorItem.color.alpha() > 0.75
           ? 'text-black'
-          : 'text-white'}"
+          : 'text-white'} border-2 {showGrouping && colorItem.group
+          ? 'border-stone-500'
+          : 'border-stone-900'}"
         style="background-color: {colorItem.declaration.value};"
       >
         <button
-          class="block w-full h-full text-start"
+          class="block w-full h-full text-start {showGrouping && colorItem.group
+            ? 'cursor-not-allowed'
+            : ''}"
           onclick={() => functionOnClick(colorItem)}
           >{colorItem.property}</button
         >
@@ -175,19 +270,72 @@
   {#if ast && rules.length === 0}
     <p>No colors found in the CSS code.</p>
   {/if}
-  <ul class="max-h-[45vh] overflow-y-auto font-mono space-y-6 break-words">
+  <ul class="max-h-[45vh] overflow-y-auto space-y-6">
     {#each rules as rule}
-      <div class="mb-2 font-semibold sticky top-0 bg-stone-800">
+      <div
+        class="mb-2 font-semibold sticky top-0 bg-stone-800 font-mono break-words"
+      >
         {rule.name}
       </div>
       {#each rule.sections as section}
         {#if section.name}
-          <div class="mb-2 text-sm">// {section.name}</div>
+          <div class="mb-2 text-sm font-mono break-words">
+            // {section.name}
+          </div>
         {/if}
-        {@render colorList(section.colorItems, addColorItemToGroup)}
+        {@render colorList(section.colorItems, addColorItemToGroup, true)}
       {/each}
     {/each}
   </ul>
+
+  {#snippet modificator(
+    checkboxName: keyof ModificationGroup,
+    valueName: keyof ModificationGroup,
+    label: string,
+    scale: number = 1
+  )}
+    {#if selectedGroup}
+      <div class="w-full flex items-center justify-between">
+        <label
+          for={checkboxName}
+          class="flex items-center gap-4 w-1/2 justify-between px-4"
+        >
+          {label}
+          <input
+            type="checkbox"
+            name={checkboxName}
+            id={checkboxName}
+            bind:checked={selectedGroup[checkboxName] as boolean}
+          />
+        </label>
+        {#if selectedGroup[checkboxName]}
+          <div class="flex items-center gap-4 w-1/2 px-4">
+            <input
+              type="range"
+              name="{valueName}Range"
+              id="{valueName}Range"
+              min="-{scale}"
+              max={scale}
+              step={scale >= 10 ? 1 : 0.01}
+              class="w-full"
+              bind:value={selectedGroup[valueName]}
+            />
+            <input
+              type="number"
+              name="{valueName}Number"
+              id="{valueName}Number"
+              min="-{scale}"
+              max={scale}
+              step={scale >= 10 ? 1 : 0.01}
+              class="bg-stone-700 w-16"
+              bind:value={selectedGroup[valueName]}
+            />
+          </div>
+        {/if}
+      </div>
+    {/if}
+  {/snippet}
+
   {#if rules.length > 0}
     <div class="h-[45vh] flex flex-col">
       <div class="flex justify-between items-start text-nowrap">
@@ -209,7 +357,7 @@
         >
       </div>
       {#if selectedGroup}
-        <div class="overflow-y-auto grow font-mono break-words">
+        <div class="overflow-y-auto grow">
           {#if selectedGroup.colorItems.length > 0}
             {@render colorList(
               selectedGroup.colorItems,
@@ -219,7 +367,24 @@
             <p>Click on some colors to add them to this group.</p>
           {/if}
         </div>
-        <div class="bg-stone-800">{selectedGroup.name}</div>
+        <div class="bg-stone-800">
+          {@render modificator(
+            'adjustHue',
+            'hueAdjustment',
+            'Adjust hue',
+            360 / 2
+          )}
+          {@render modificator(
+            'adjustSaturation',
+            'saturationAdjustment',
+            'Adjust saturation'
+          )}
+          {@render modificator(
+            'adjustLightness',
+            'lightnessAdjustment',
+            'Adjust lightness'
+          )}
+        </div>
       {:else}
         <p>Create a group.</p>
       {/if}
